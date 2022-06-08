450c135963a64ce881227b81bb545a1bc92ebf3f
DucllOwO <nguyenduc147862@gmail.com>
2022-06-08 23:36:09 +0700

    add login, signup with email verify

--- app/src/main/java/com/nmuddd/foodrecipeapp/view/login/LoginTabFragment.java
+++ app/src/main/java/com/nmuddd/foodrecipeapp/view/login/LoginTabFragment.java
@@ -1,5 +1,6 @@
 package com.nmuddd.foodrecipeapp.view.login;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,10 +8,17 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.nmuddd.foodrecipeapp.R;
+import com.nmuddd.foodrecipeapp.view.home.HomeActivity;
 
 public class LoginTabFragment extends Fragment {
 
@@ -19,6 +27,8 @@ public class LoginTabFragment extends Fragment {
     TextView forgot_password;
     Button login_button;
     float v = 0;
+
+    private FirebaseAuth mAuth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,6 +53,30 @@ public class LoginTabFragment extends Fragment {
         password_et.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
         forgot_password.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
         login_button.animate().translationY(0).alpha(1).setDuration(1000).setStartDelay(400).start();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (email_et.getText().toString() == "" || password_et.getText().toString() == "")
+                    Toast.makeText(getContext(), "Input information!!!", Toast.LENGTH_SHORT).show();
+                else
+                    mAuth.signInWithEmailAndPassword(email_et.getText().toString(), password_et.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful())
+                                        if (mAuth.getCurrentUser().isEmailVerified())
+                                            startActivity(new Intent(getContext(), HomeActivity.class));
+                                        else
+                                            Toast.makeText(getContext(), "Please verify your email address", Toast.LENGTH_SHORT).show();
+                                    else
+                                        Toast.makeText(getContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+            }
+        });
 
         //setupAnimate();
         // Inflate the layout for this fragment
