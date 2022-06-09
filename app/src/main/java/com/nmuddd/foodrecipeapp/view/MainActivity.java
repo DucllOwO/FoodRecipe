589a39f084b1ef515dc13494d18a2d37a98a7b02
Phuc Tran <tr.ph3322@gmail.com>
2022-06-10 02:36:22 +0700

    add main fragments and logic code to transfer

--- app/src/main/java/com/nmuddd/foodrecipeapp/view/MainActivity.java
+++ app/src/main/java/com/nmuddd/foodrecipeapp/view/MainActivity.java
@@ -1,26 +1,62 @@
 package com.nmuddd.foodrecipeapp.view;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.nmuddd.foodrecipeapp.R;
+import com.nmuddd.foodrecipeapp.adapter.ViewPagerMainAdapter;
 
 import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    @BindView(R.id.view_pager2) private ViewPager2 viewPager2;
-//    @BindView(R.id.bottom_nav) private BottomNavigationView bottomNavigationView;
+    private ViewPager2 viewPager2;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        viewPager2 = findViewById(R.id.view_pager2);
+        bottomNavigationView = findViewById(R.id.bottom_nav);
 
+        ViewPagerMainAdapter viewPagerMainAdapter = new ViewPagerMainAdapter(this);
+        viewPager2.setAdapter(viewPagerMainAdapter);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.action_home:
+                        viewPager2.setCurrentItem(0);
+                        bottomNavigationView.getMenu().findItem(R.id.action_home).setChecked(true);
+                        break;
+                    case R.id.action_favorite:
+                        viewPager2.setCurrentItem(1);
+                        bottomNavigationView.getMenu().findItem(R.id.action_favorite).setChecked(true);
+                        break;
+                    case R.id.action_personal:
+                        viewPager2.setCurrentItem(2);
+                        bottomNavigationView.getMenu().findItem(R.id.action_personal).setChecked(true);
+                        break;
+                    case R.id.action_account:
+                        viewPager2.setCurrentItem(3);
+                        bottomNavigationView.getMenu().findItem(R.id.action_account).setChecked(true);
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/nmuddd/foodrecipeapp/view/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.nmuddd.foodrecipeapp.view.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.nmuddd.foodrecipeapp.R;
+
+public class HomeFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}
